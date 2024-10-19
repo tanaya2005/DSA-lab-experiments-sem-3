4a18ce8fd546288f23e126928f36d36f513037c4
tanaya2005 <tanayaitis@gmail.com>
2024-10-20 00:14:04 +0530

    exp 14: sorting algos_2.c
    1. quick sort
    2.  merge sort
    time complexity O(nlogn)

---
+++ exp 14: sorting algos_2.c
@@ -0,0 +1,127 @@
+#include <stdio.h>
+
+void print(int *arr, int n)
+{
+    for (int i = 0; i < n; i++)
+    {
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
+}
+
+void merge(int *arr, int l, int m, int r)
+{
+    int n1 = m - l + 1;
+    int n2 = r - m;
+    int L[n1], R[n2];
+    for (int i = 0; i < n1; i++)
+        L[i] = arr[l + i];
+    for (int i = 0; i < n2; i++)
+        R[i] = arr[m + 1 + i];
+    int i = 0, j = 0, k = l;
+    while (i < n1 && j < n2)
+    {
+        if (L[i] <= R[j])
+        {
+            arr[k] = L[i];
+            i++;
+        }
+        else
+        {
+            arr[k] = R[j];
+            j++;
+        }
+        k++;
+    }
+    while (i < n1)
+    {
+        arr[k] = L[i];
+        i++;
+        k++;
+    }
+    while (j < n2)
+    {
+        arr[k] = R[j];
+        j++;
+        k++;
+    }
+}
+
+void merge_sort(int *arr, int l, int r)
+{
+    if (l < r)
+    {
+        int m = l + (r - l) / 2;
+        merge_sort(arr, l, m);
+        merge_sort(arr, m + 1, r);
+        merge(arr, l, m, r);
+    }
+}
+
+int partition(int *arr, int low, int high)
+{
+    int pivot = arr[high];
+    int i = (low - 1);
+    for (int j = low; j < high; j++)
+    {
+        if (arr[j] < pivot)
+        {
+            i++;
+            int temp = arr[i];
+            arr[i] = arr[j];
+            arr[j] = temp;
+        }
+    }
+    int temp = arr[i + 1];
+    arr[i + 1] = arr[high];
+    arr[high] = temp;
+    return (i + 1);
+}
+
+void quick_sort(int *arr, int low, int high)
+{
+    if (low < high)
+    {
+        int pi = partition(arr, low, high);
+        quick_sort(arr, low, pi - 1);
+        quick_sort(arr, pi + 1, high);
+    }
+}
+
+int main()
+{
+    int arr[] = {8, 0, 1, 7, 3};
+    int n = 5;
+    int choice;
+
+    do
+    {
+        printf("Choose sorting method:\n");
+        printf("1. Merge Sort\n");
+        printf("2. Quick Sort\n");
+        printf("3. Exit\n");
+        printf("Enter choice: ");
+        scanf("%d", &choice);
+
+        switch (choice)
+        {
+        case 1:
+            merge_sort(arr, 0, n - 1);
+            printf("Array after Merge Sort: ");
+            print(arr, n);
+            break;
+        case 2:
+            quick_sort(arr, 0, n - 1);
+            printf("Array after Quick Sort: ");
+            print(arr, n);
+            break;
+        case 3:
+            printf("Exiting...\n");
+            break;
+        default:
+            printf("Invalid choice, try again.\n");
+        }
+    } while (choice != 3);
+
+    return 0;
+}
